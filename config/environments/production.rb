c823e7764f600c0fb1e70f34a74652fac44a07da
Rahul <jain.rahul1095@gmail.com>
2017-04-07 19:52:36 +0530

    change productin environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'https://still-beach-90231.herokuapp.com'}
 end
